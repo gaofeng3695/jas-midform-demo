ffdcd635ce326bf25cb70e034876b735b9243441
gaofeng@jasgroup.cn <gaofeng@jasgroup.cn>
2021-03-01 16:50:45 +0800

    表单生成--添加表单项复制功能及删除功能，优化表单项操作之后的默认活跃显示；优化拖拽操作交互；

--- src/views/module-page-maker/form-diy/FormDiy.vue
+++ src/views/module-page-maker/form-diy/FormDiy.vue
@@ -7,7 +7,7 @@
     <div class="formDiy">
       <div class="leftbox">
         <div class="container">
-          <draggable class="dragArea list-group" :list="typelist" :group="{ name: 'people', pull: 'clone', put: false }" :clone="cloneDog" @change="log">
+          <draggable class="dragArea list-group" :list="typelist" :group="{ name: 'people', pull: 'clone', put: false }" :clone="createNewItem" @change="log">
             <div class="list-group-item01" v-for="element in typelist" :key="element.type">
               <div class="item">
                 {{ element.name }}
@@ -19,14 +19,17 @@
       </div>
       <div class="centerbox">
         <div class="container">
-          <el-form label-position="left" label-width="80px">
+          <el-form label-position="left" label-width="80px" style="width:100%;height:100%;">
             <draggable class="dragArea list-group" :list="formitem" group="people" @change="log">
               <div class="list-group-item" v-for="item in formitem" :key="item.field">
                 <div class="item2wrap">
                   <div class="item2" @click="activeItem =item" :class="activeItem == item ? 'active' : ''">
                     <!-- {{ item.name }} -->
                     <JasFormItem v-model="form[item.field]" :config="item"></JasFormItem>
-
+                    <div v-show="activeItem == item" class="item2tool">
+                      <el-button type="primary" @click.stop="copyItem(item)" size="mini" icon="fa fa-clone" plain round></el-button>
+                      <el-button type="danger" @click.stop="deleteItem(item)" size="mini" icon="fa fa-trash" plain round></el-button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -35,8 +38,8 @@
         </div>
       </div>
       <div class="rightbox">
-        <div class="container">
-          <el-form label-position="left" label-width="80px">
+        <div class="container rightcon">
+          <el-form label-position="left" label-width="60px">
             <el-form-item v-for="(item,key) in activeItem" :key="key" :label="key">
               <el-input v-model="activeItem[key]" size="small" clearable></el-input>
             </el-form-item>
@@ -94,12 +97,33 @@ export default {
     log: function (evt) {
       window.console.log(evt);
     },
-    cloneDog(item) {
-      return {
+    createNewItem(item) {
+      var newItem = {
         name: item.name,
         type: item.type,
         field: "name" + this.idGlobal++,
       };
+      this.activeItem = newItem;
+      return newItem;
+    },
+    copyItem(item) {
+      var that = this;
+      let index = this.formitem.indexOf(item);
+      let newItem = {
+        ...item,
+        field: "name" + this.idGlobal++,
+      };
+      this.formitem.splice(index + 1, 0, newItem);
+      this.activeItem = newItem;
+    },
+    deleteItem(item) {
+      let index = this.formitem.indexOf(item);
+      this.formitem.splice(index, 1);
+      if (this.formitem.length > 0) {
+        this.activeItem = this.formitem[index] || this.formitem[index - 1];
+      } else {
+        this.activeItem = null;
+      }
     },
   },
 };
@@ -128,6 +152,7 @@ export default {
   .container {
     border-radius: 6px;
     border: 1px solid #ececec;
+    box-sizing: border-box;
     height: 100%;
   }
   .leftbox {
@@ -141,6 +166,12 @@ export default {
   .rightbox {
     width: 350px;
     padding: 10px;
+    .rightcon {
+      padding: 10px;
+      .el-form-item {
+        margin-bottom: 6px;
+      }
+    }
   }
 }
 .dragArea {
@@ -171,6 +202,7 @@ export default {
     padding-left: 9px;
     cursor: move;
     overflow: hidden;
+    position: relative;
     &:hover {
       background: #f3faff;
     }
@@ -178,6 +210,13 @@ export default {
       padding-left: 6px;
       background: #e0f3ff !important;
       border-left: 4px solid #7cc9fd;
+    }
+    .item2tool {
+      height: 30px;
+      width: 100px;
+      position: absolute;
+      right: 10px;
+      top: 12px;
     }
   }
 }
